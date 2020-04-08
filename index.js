617faf130a4e39a5fa42c69236630a7bee2923f6
Nathan Gabriel <nathangabriel27@gmail.com>
2020-04-07 23:27:45 -0300

    renomeando o branch

---
+++ index.js
@@ -0,0 +1,14 @@
+var nome;
+nome = "Nathan "
+var idade = 21
+var pretencaoSalarial = 3277.22
+var veleRefeicao =true
+console.log(nome);
+
+var pessoa = []
+pessoa.sobrenome = "gabriel"
+pessoa.joventude = true
+pessoa.nome = "nathan"
+console.log(pessoa);
+console.log("_____",pessoa.joventude);
+
